333ff7b64227579cce53465e5c5030645cafef37
Adrian Ortega <adrian.ortega@vincit.fi>
2019-05-04 20:25:19 -0700

    adds different backgrounds

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import MemphisParticles from '@/MemphisParticles'
+import {getRandomFromArray} from '@/util'
 
+const backgroundColors = ['#e3ffe4', '#fffee4', '#dde5ff', '#fff2f9']
 const urlParams = new URLSearchParams(window.location.search);
-const count = urlParams.get('particles') || 300;
-const app = new MemphisParticles('#canvas-particles', count, '#e3ffe4');
+const count = urlParams.get('particles') || (window.innerWidth < 1024 ? 90 : 300);
+const app = new MemphisParticles('#canvas-particles', count, getRandomFromArray(backgroundColors));
 
 window.MP = app;
 document.addEventListener('DOMContentLoaded', () => app.init())
